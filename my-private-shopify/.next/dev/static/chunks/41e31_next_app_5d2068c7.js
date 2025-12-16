5f998a96aa5a138494ef1dabb464252e302a376e
kk123487 <caylim12345@gmail.com>
2025-12-16 21:39:34 +0200

    Remove node_modules and .next from git tracking and add to .gitignore

--- my-private-shopify/.next/dev/static/chunks/41e31_next_app_5d2068c7.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/my-private-shopify/node_modules/next/app.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/my-private-shopify/node_modules/next/dist/pages/_app.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=41e31_next_app_5d2068c7.js.map
--- my-private-shopify/.next/dev/static/chunks/41e31_next_error_3603d566.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/my-private-shopify/node_modules/next/error.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/my-private-shopify/node_modules/next/dist/pages/_error.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=41e31_next_error_3603d566.js.map
